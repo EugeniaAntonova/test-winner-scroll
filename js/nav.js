566f57ad2b97f4830f86e9a9457ab791965c9890
EvgeniiaAntonova <alexocean.e.k@gmail.com>
2024-02-19 09:25:29 +0600

    feat: added condition to not show empty current user if the one does not participate

--- js/nav.js
+++ js/nav.js
@@ -8,11 +8,14 @@ const winnerTemplate = document.querySelector('#winner-template').content;
 const winnersList = document.querySelector('.winners-list');
 
 const createPlayer = (data) => {
-    const { nick, place, score } = data;
+    const { nick, place, score, bgUserToken } = data;
     const player = playerTemplate.cloneNode(true);
     player.querySelector('.nick').textContent = nick;
     player.querySelector('.place').textContent = place;
     player.querySelector('.score').textContent = score;
+    // if (parseInt(place) <= 5) {
+    //     player.querySelector('.avatar-img').src = `https://sz.kz/picture?bgAvatar=${bgUserToken}`
+    // }
 
     return player;
 }
@@ -26,35 +29,24 @@ const createWinner = (data) => {
 }
 
 const createAnchor = (current) => {
-    let players = playersList.children;
-    players[current.place - 1].id = 'current-in-the-list';
-    players[current.place - 1].classList.add('current-player')
-    
-    const anchor = document.createElement('a');
-    anchor.id = 'scroll-to-me-link';
-    anchor.href = '#current-in-the-list';
-    
-    if (current.place <= 100 && current.place > 5) {
-        currentPlayerAction.append(anchor);
-        currentPlayerAction.classList.add('ready');
-
-        const observer = new IntersectionObserver((entries) => {
-            if (entries[0].isIntersecting) {
-                currentPlayerAction.classList.remove('ready');
-            }
-        }, {threshold: 1, rootMargin: "-150px",});
+    if (current) {
+        let players = playersList.children;
+        players[current.place - 1].id = 'current-in-the-list';
+        players[current.place - 1].classList.add('current-player')
         
-        window.addEventListener('scroll', () => {
-            if (window.scrollY < 500) {
-                currentPlayerAction.classList.add('ready'); 
-            }
-            
-            observer.observe(document.querySelector('#current-in-the-list'));
-        })
+        const anchor = document.createElement('a');
+        anchor.id = 'scroll-to-me-link';
+        anchor.href = '#current-in-the-list';
+        
+        if (current.place <= 100 && current.place > 5) {
+            currentPlayerAction.append(anchor);
+            currentPlayerAction.classList.add('ready');
+        }
+    } else {
+        currentPlayerContainer.style.display = 'none';
     }
 }
 
-// не забыть сфетчить предыдущих победителей!!!!!!!
 const onSuccessPlayers = (data, current, cb) => {
     data.forEach(element => {
         const player = cb(element);
@@ -89,7 +81,7 @@ const getData = async (onSuccess, onFail, cb) => {
 
         const resp = await response.json();
         const data = resp.data;
-        const current = resp.currentUser;
+        const current = resp.currentUser ? resp.currentUser : false;
         onSuccess(data, current, cb);
     } catch (error) {
         onFail(error.message);
